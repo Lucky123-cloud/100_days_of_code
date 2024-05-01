7f857532c0dc5f93a12c8bbf827b29ed10c99c44
Lucky-cloud 123 <luckybaraka@gmail.com>
2024-05-01 20:20:28 +0300

    added a few details

--- 30_days_Js_problems/longest_word.js
+++ 30_days_Js_problems/longest_word.js
@@ -22,3 +22,32 @@ function longestWord(sentence) {
 }
 const sentence = "The quick brown fox jumps over the lazy dog";
 console.log(longestWord(sentence));
+
+//You are probably asking yourself why is that there are some letters that have the same numbers of letters eg quick and brown
+//but yet only the first one is taken, that is because that is how the algorithm works to pick the fisrt one appeared
+
+//if we want to make it return all the longest words with the same number of characters we will twick our code like this below:
+
+
+function longestWords(sentence) {
+    const words = sentence.split(" ");
+    let longestLength = 0;
+    let longestWords = [];
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        if (word.length > longestLength) {
+            longestLength = word.length;
+            longestWords = [word];
+        } else if (word.length === longestLength) {
+            longestWords.push(word);
+        }
+    }
+
+    return longestWords;
+}
+
+// Example usage:
+const sentence = "The quick brown fox jumps over the lazy dog";
+console.log(longestWords(sentence)); // Output: ["quick", "brown"]
+
