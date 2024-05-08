d69d22b21bb9d2ea967ef28332831c81de0a06c1
Lucky-cloud 123 <luckybaraka@gmail.com>
2024-05-08 20:32:26 +0300

    I added the files in this problem

---
+++ 30_days_Js_problems/areAnagrams.js
@@ -0,0 +1,22 @@
+// Write a function to check if two strings are anagrams of each other.
+
+// An anagram is a word or phrase formed by rearranging the letters of a different word or phrase
+
+Input: "listen", "silent"
+Output: true
+
+function areAnagrams(str1, str2) {
+    str1 = str1.replace(/[^a-z]/g, '').toLowerCase();
+    str2 = str2.replace(/[^a-z]/g, '').toLowercase();
+
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    const sortedStr1 = str1.split('').sort().join('');
+    const sortedStr2 = str1.split('').sort().join('');
+
+    return sortedStr1 === sortedStr2;
+}
+
+console.log(areAnagrams("listen", "silent"))
